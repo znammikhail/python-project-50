619534b4b15e0eaca6e4280f79ce8d9d3ed198cb
znammikhail <znamenskymihail@gmail.com>
2022-09-16 00:31:38 +0300

    create gendiff v0

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/znammikhail/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/znammikhail/python-project-50/actions)
+
+
+<a href="https://asciinema.org/a/2ClGer9QmLEaX0A98pp4FPJsL" target="_blank"><img src="https://asciinema.org/a/2ClGer9QmLEaX0A98pp4FPJsL.svg" /></a>
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,22 +1,46 @@
 # -*- coding:utf-8 -*-
+""""""
+import os
 import argparse
-"""usage: gendiff [-h] first_file second_file
+import json
+import pathlib
 
-Compares two configuration files and shows a difference.
 
-positional arguments:
-  first_file
-  second_file
+def generate_diff(filepath1, filepath2):
+    """Generate difference files."""
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path_1 = pathlib.Path(dir_path, filepath1)
+    file_path_2 = pathlib.Path(dir_path, filepath2)
+    file_1 = json.load(open(file_path_1))   # dict
+    file_2 = json.load(open(file_path_2))
+    file_3 = {}
 
-optional arguments:
-  -h, --help            show this help message and exit"""
+    for key1 in file_1.keys():
+        if key1 in file_2.keys():
+            if file_1[key1] == file_2[key1]:
+                file_3[f'  {key1}'] = file_1[key1]
+            else:
+                file_3[f'- {key1}'] = file_1[key1]
+                file_3[f'+ {key1}'] = file_2[key1]
+        else:
+            file_3[f'- {key1}'] = file_1[key1]
+    for key2 in file_2.keys():
+        if key2 not in file_1.keys():
+            file_3[f'+ {key2}'] = file_2[key2]
+
+    file_3 = dict(sorted(file_3.items(), key=lambda item: item[0][2:]))
+    return json.dumps((file_3), indent=4)
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration \
+        files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-    parser.parse_args()
+    parser.add_argument('-f', '--format', help='set format of output')
+    args = parser.parse_args()
+    first_file, second_file = args.first_file, args.second_file
+    print(generate_diff(first_file, second_file))
 
 
 if __name__ == '__main__':
