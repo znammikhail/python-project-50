dc2a4d7a766c0ddf3310bb73436685e358a29b76
znammikhail <znamenskymihail@gmail.com>
2022-09-15 16:12:29 +0300

    first commit

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+import argparse
+"""usage: gendiff [-h] first_file second_file
+
+Compares two configuration files and shows a difference.
+
+positional arguments:
+  first_file
+  second_file
+
+optional arguments:
+  -h, --help            show this help message and exit"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
