d6f19c968c1ae9c78ab669b851a27c2d00abcc7f
znammikhail <znamenskymihail@gmail.com>
2023-02-21 16:35:13 +0300

    update output

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 [![Actions Status](https://github.com/znammikhail/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/znammikhail/python-project-50/actions)
 
 
-<a href="https://asciinema.org/a/2ClGer9QmLEaX0A98pp4FPJsL" target="_blank"><img src="https://asciinema.org/a/2ClGer9QmLEaX0A98pp4FPJsL.svg" /></a>
+<a href="https://asciinema.org/a/zt9qVpKCqBS8dzhvjUVdHLOI9" target="_blank"><img src="https://asciinema.org/a/zt9qVpKCqBS8dzhvjUVdHLOI9.svg" /></a>
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,24 +12,25 @@ def generate_diff(filepath1, filepath2):
     file_path_1 = pathlib.Path(dir_path, filepath1)
     file_path_2 = pathlib.Path(dir_path, filepath2)
     file_1 = json.load(open(file_path_1))   # dict
-    file_2 = json.load(open(file_path_2))
+    file_1 = dict(sorted(file_1.items()))
+    file_2 = json.load(open(file_path_2))   
+    file_2 = dict(sorted(file_2.items()))
     file_3 = {}
 
     for key1 in file_1.keys():
         if key1 in file_2.keys():
             if file_1[key1] == file_2[key1]:
-                file_3[f'  {key1}'] = file_1[key1]
+                file_3[f"  {key1}"] = file_1[key1]
             else:
-                file_3[f'- {key1}'] = file_1[key1]
-                file_3[f'+ {key1}'] = file_2[key1]
+                file_3[f"- {key1}"] = file_1[key1]
+                file_3[f"+ {key1}"] = file_2[key1]
         else:
             file_3[f'- {key1}'] = file_1[key1]
     for key2 in file_2.keys():
         if key2 not in file_1.keys():
             file_3[f'+ {key2}'] = file_2[key2]
 
-    file_3 = dict(sorted(file_3.items(), key=lambda item: item[0][2:]))
-    return json.dumps((file_3), indent=4)
+    return json.dumps((file_3), indent=4).replace('"', '')  # str
 
 
 def main():
